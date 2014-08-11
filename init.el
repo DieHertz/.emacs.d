615d0c6bc832dd1d4702ec745731433b82f10f30
DieHertz <diehertz@gmail.com>
2014-08-11 14:21:39 +0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,38 @@
+(require 'package)
+
+(setq package-archives '(
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("elpa" . "http://tromey.com/elpa/")
+			 ("melpa" . "http://melpa.milkbox.net/packages")
+			 ("org" . "http://orgmode.org/elpa")))
+
+;; ensure every single package installed via Package.el is loaded in ready to be customized
+(package-initialize)
+
+(defconst custom-file-path (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file-path) (write-region "" nil custom-file-path))
+
+(setq custom-file custom-file-path)
+(load custom-file-path)
+
+;; my once-and-for-all favourite theme
+(require 'monokai-theme)
+(load-theme 'monokai)
+
+;; handy plugin for moving entire lines up/down (like C-S-ArrowUp/ArrowDown in Sublime Text)
+(require 'move-text)
+(move-text-default-bindings)
+
+(require 'ido)
+(ido-mode)
+(setq ido-enable-flex-match)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-linum-mode)
+(column-number-mode)
+(show-paren-mode 1)
+(setq make-backup-files nil)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
